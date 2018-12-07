719a613f95bcaae3c2c94810882e0622c41b979a
Dragan Milic <dragan@netice9.com>
2018-12-07 13:12:43 +0000

    use contract path to find solidity sources (#20)

--- contract_test.go
+++ contract_test.go
@@ -18,7 +18,7 @@ func TestContract(t *testing.T) {
 	var owner = ethertest.NewAccount()
 
 	tr.AddGenesisAccountAllocation(owner.Address(), ethertest.EthToWei(100))
-	tr.AddCoverageForContracts("./test/build/test/combined.json", "test/contracts/test.sol")
+	tr.AddCoverageForContracts("./test/build/test/combined.json", "test/contracts", []string{"test.sol"})
 
 	require := require.New(t)
 	be := tr.NewTestBackend(ethertest.WithBlockGasLimit(8000000), ethertest.WithBlockchainTime(time.Now().Add(-24*time.Hour)))
